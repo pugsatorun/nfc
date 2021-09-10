fdf1b19c81935ac4e5d422156b5d90ab909f8673
pugsatoshi <satoshi.ito.dev@gmail.com>
2021-09-10 15:04:40 +0900

    repair

--- sheetwrite.py
+++ sheetwrite.py
@@ -4,7 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('python-write-test-325506-1c7f8907c4ec.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('../python-write-test-325506-04f791ddf6a9.json', scope)
 gc = gspread.authorize(credentials)
 wks = gc.open('python 書き込みテスト').sheet1
 
